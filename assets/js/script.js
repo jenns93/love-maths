031cf5c8d6f73ee12ffaff766e0b11cf4f252783
jenns93 <jjenns93@gmail.com>
2021-06-30 00:48:47 +0000

    display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,29 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
+
 })
 
 /**
  * The maingame "loop", called when script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(){
+function runGame(gameType){
     let num1 = Math.floor(Math.random() * 24) + 1;
     let num2 = Math.floor(Math.random() * 24) + 1;
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unkown game type: ${gameType}`);
+        throw `Unkown game type: ${gameType}.Aborting!`;
+    }
 }
 function checkAnswer(){
     
@@ -33,12 +43,24 @@ function incrementScore(){
 function incrementWrongAnswer(){
     
 }
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+
+}
+function displaySubtractQuestion(operand1, operand2){
+
+     document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "-";
     
 }
-function displaySubtractQuestion(){
-    
-}
-function displayMultiplyQuestion(){
+function displayMultiplyQuestion(operand1, operand2){
+
+     document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
     
 }
