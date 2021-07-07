612c22057d6609a236bad0b1b6cb6985e407c303
jenns93 <jjenns93@gmail.com>
2021-07-07 14:51:08 +0000

    add functions to test

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,8 +1,20 @@
   var myArray = ["Rock","Paper","Scissors","Lizard","Spock"];
   var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
   var modal = document.getElementById("myModal");
-  
-function send(){
+function tester111(){
+  var timeleft = 5;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("computer").innerHTML ="Go!";
+  } else {
+    document.getElementById("computer").innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
+}
+
+function send1(){
   var line = "";
   var name = document.getElementById("name").value;
   let streak = document.getElementById("streak").innerHTML;
@@ -23,9 +35,11 @@ function counter(){
   let loseCheck =document.getElementById("lose").innerHTML;
   if (loseCheck >1){
     modal.style.display = "block";
+    sendit();
     reset();
     document.getElementById('level').innerHTML="1";
     document.getElementById('streak').innerHTML="0";
+    
     
   }
   }
@@ -52,15 +66,31 @@ function counter(){
     document.getElementById('draw').innerHTML="0";
 
   }
+function newcomputer(){
+  var computer00 = myArray[Math.floor(Math.random()*myArray.length)];
+  var timeleft = 3;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("computer").innerHTML =computer00;
+  } else {
+    document.getElementById("computer").innerHTML = timeleft ;
+  }
+  timeleft -= 1;
+}, 1000);
+
+}
+
  function rock(){
-  let computer = document.getElementById("computer").innerHTML = myArray[Math.floor(Math.random()*myArray.length)];
+ let computer001 = document.getElementById('computer');
   let rock = document.getElementById('output');
   rock.innerHTML = document.getElementById('rock').innerHTML;
   let score = document.getElementById("score");
-  if (computer === myArray[0]) { score.innerHTML= "draw"; }
-  else if (computer === myArray[1]) { score.innerHTML= "you lose"; }
-  else if (computer === myArray[2]) { score.innerHTML= "you win"; }
-  else if (computer === myArray[3]) { score.innerHTML= "you win"; }
+  newcomputer();
+  if (computer001 === myArray[0]) { score.innerHTML= "draw"; }
+  else if (computer001 === myArray[1]) { score.innerHTML= "you lose"; }
+  else if (computer001 === myArray[2]) { score.innerHTML= "you win"; }
+  else if (computer001 === myArray[3]) { score.innerHTML= "you win"; }
   else  { score.innerHTML= "you lose"; }
   incrementScore();
   gameOver();
@@ -68,7 +98,7 @@ function counter(){
   counter();
  }
  function paper(){
-  let computer = document.getElementById("computer").innerHTML = myArray[Math.floor(Math.random()*myArray.length)];
+  let computer = document.getElementById("computer").innerHTML = newcomputer();
   let paper = document.getElementById('output');
   paper.innerHTML = document.getElementById('paper').innerHTML;
   let score = document.getElementById("score");
@@ -134,4 +164,51 @@ function counter(){
   else if (score.innerHTML ==="draw"){++draw.innerHTML}
   else {alert="error"}
 
+}
+
+
+function addData(el) {
+  var table = document.getElementById('list');
+  var tr = table.insertRow();
+  el.form.querySelectorAll('input').forEach(function(el) {
+    var cell = tr.appendChild(document.createElement('td'));
+    cell.textContent = el.value;
+  });
+}
+function sendit(){
+  let yourScore = document.getElementById('totalamount');
+  yourScore.value=streak.innerHTML;
+}
+
+function getFormDetails(event){
+  event.preventDefault();
+  console.log("this");
+   let name=document.getElementById("name");
+   console.log(name.value);
+   let email=document.getElementById("email");
+    console.log(email.value);
+  let nameResult = document.getElementById('name-result').innerHTML=name.value;
+   console.log(nameResult);
+    let emailResult = document.getElementById('email-result').innerHTML=email.value;
+   console.log(emailResult);
+}
+let newsletterForm=document.getElementById("newsletter-signup");
+newsletterForm.addEventListener("submit", getFormDetails);
+
+function Send ()
+ {
+     var x = document.getElementById("Box");
+     var Select_value = x.options[x.selectedIndex].text;
+      
+      var newOpt = document.createElement('option');
+      newOpt.innerHTML = Select_value;
+      document.getElementById('Box2').appendChild(newOpt);
+ }
+function close(){
+  let modal = document.getElementById('myModal');
+  if(document.getElementById('bouton_Envoyer').clicked == true)
+{
+   
+  modal.style.display = "none";
+}
 }
